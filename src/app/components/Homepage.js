aced689e271f73d51bbaae2f588048f431d14db5
Cameron Hilliard <cameronhilliard@outlook.com>
2025-01-22 14:42:35 -0400

    new commit

--- src/app/components/Homepage.js
+++ src/app/components/Homepage.js
@@ -2,9 +2,6 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import {LinkedinShareButton, LinkedinIcon, EmailShareButton, EmailIcon}  from 'next-share'
-import Projectcard from './projectcard'
-
 
 const Homepage = () => {
   
@@ -21,26 +18,38 @@ const Homepage = () => {
             className='mb-5 border-2 rounded-full border-accent'
           />
         </div>
-        <h1 className='font-mont lg:text-5xl md:text-3xl text-2xl  text-accent mx-auto dark:text-white'>Hi, I'm <strong className='text-accent'>Cameron Hilliard</strong></h1> 
-        <p className='font-mont text-accent dark:text-white mx-auto lg:text-2xl text-lg  col-start-1  mt-4 '> Software Engineer / IT Professional</p>
-        <p className='font-mont lg:text-xl text-accent dark:text-white mx-auto  mt-4 mb-4 col-start-1 italic'> "Turning Ideas into Reality."</p>
-        <div className='flex mx-auto col-start-2 mt-4'>
-               <Link href="#projectspage" className='bg-accent rounded-lg mr-4 p-4 font-mont  lg:text-lg font-bold'>View My work</Link> 
-               <Link href="#contactme" className='bg-accent rounded-lg p-4 font-mont lg:text-lg font-bold'>Contact Me</Link>
-          </div>
-        </div> 
+        <h1 className='font-mont lg:text-5xl md:text-3xl text-2xl text-accent mx-auto dark:text-white'>
+          Hi, I'm <strong className='text-accent'>Cameron Hilliard</strong>
+        </h1>
+        <p className='font-mont text-accent dark:text-white mx-auto lg:text-2xl text-lg mt-4'>
+          Software Engineer / IT Professional
+        </p>
+        <p className='font-mont lg:text-xl text-accent dark:text-white mx-auto mt-4 mb-4 italic'>
+          "Turning Ideas into Reality."
+        </p>
+        <div className='flex mx-auto mt-4'>
+          <Link href="#projectspage" className='bg-accent rounded-lg mr-4 p-4 font-mont lg:text-lg font-bold'>
+            View My work
+          </Link> 
+          <Link href="#contactme" className='bg-accent rounded-lg p-4 font-mont lg:text-lg font-bold'>
+            Contact Me
+          </Link>
+        </div>
+      </div> 
     </section>
 
     <section id="aboutpage" className="lg:w-2/3 md:w-4/5 py-32 p-6 text-white mx-auto">
       <h2 className="text-3xl text-center font-mont font-semibold text-accent">About Me</h2>
-      <p className="mt-4 lg:text-2xl lg:w-full md:max-w-2xl text-center mx-auto text-lg font-mont text-accent dark:text-white">I am an aspiring Software Engineer based in Halifax, Nova Scotia, with a passion for developing innovative digital solutions that improve efficiency and simplify everyday processes. My goal is to create impactful technology that makes life easier.</p>
+      <p className="mt-4 lg:text-2xl lg:w-full md:max-w-2xl text-center mx-auto text-lg font-mont text-accent dark:text-white">
+        I am an aspiring Software Engineer based in Halifax, Nova Scotia, with a passion for developing innovative digital solutions that improve efficiency and simplify everyday processes. My goal is to create impactful technology that makes life easier.
+      </p>
     </section>
 
-    <section id="skillspage" className="lg:w-2/3 md:w-4/5 p-6 py-24  text-white mx-auto  ">
+    <section id="skillspage" className="lg:w-2/3 md:w-4/5 p-6 py-24 text-white mx-auto">
       <h2 className="text-3xl text-center font-mont font-semibold dark:text-accent text-accent">Skills</h2>
-      <div className='lg:flex lg:flex-row  flex-wrap lg:mt-20 flex flex-col'>
-        <div className='lg:col-start-2 md:col-start-1  md:p-0 p-2 mx-auto'>
-        <Image 
+      <div className='lg:flex lg:flex-row flex-wrap lg:mt-20 flex flex-col'>
+        <div className='lg:col-start-2 md:col-start-1 md:p-0 p-2 mx-auto'>
+          <Image 
             src={"/images/react.png"}
             width={90}
             height={80}
@@ -50,7 +59,7 @@ const Homepage = () => {
         </div>
 
         <div className='mx-auto pt-4'>
-        <Image 
+          <Image 
             src={"/images/js.png"}
             width={90}
             height={80}
@@ -59,10 +68,8 @@ const Homepage = () => {
           <p className='mx-auto text-accent dark:text-white text-center font-mont'>Javascript</p>
         </div>
 
-
-
         <div className='mx-auto pt-4'>
-        <Image 
+          <Image 
             src={"/images/nodejs.png"}
             width={90}
             height={80}
@@ -71,9 +78,8 @@ const Homepage = () => {
           <p className='mx-auto text-accent dark:text-white text-center font-mont'>Node.js</p>
         </div>
 
-
         <div className='mx-auto pt-4'>
-        <Image 
+          <Image 
             src={"/images/mongodb.png"}
             width={90}
             height={80}
@@ -82,8 +88,8 @@ const Homepage = () => {
           <p className='mx-auto text-accent dark:text-white text-center font-mont'>MongoDB</p>
         </div>
 
-        <div className=' mx-auto pt-4'>
-        <Image  
+        <div className='mx-auto pt-4'>
+          <Image  
             src={"/images/python.png"}
             width={90}
             height={80}
@@ -92,9 +98,8 @@ const Homepage = () => {
           <p className='mx-auto text-accent dark:text-white p-2 text-center font-mont'>Python</p>
         </div>
 
-
         <div className='mx-auto pt-4'>
-        <Image  
+          <Image  
             src={"/images/tailwind.png"}
             width={90}
             height={80}
@@ -102,24 +107,26 @@ const Homepage = () => {
           />
           <p className='mx-auto p-2 text-accent dark:text-white text-center font-mont'>Tailwind</p>
         </div>
-        </div>
-      </section>
+      </div>
+    </section>
 
-
-      <section id="projectspage" className="lg:w-2/3 md:w-4/5 py-24 p-6 text-white mx-auto">
-        <h2 className="text-3xl text-center font-mont font-semibold text-accent">Projects</h2>
-        <p className="mt-4 lg:text-2xl lg:w-full md:max-w-2xl text-center mx-auto text-lg font-mont text-accent dark:text-white">Create Project Component Next.</p>
-        <div className='lg:grid lg:grid-cols-2 flex flex-col'>
-          <div className="bg-zinc-800 lg:m-8 lg:p-4 m-4 p-4 font-mont rounded">
-            <h2 className="text-accent font-bold text-xl">Project #1</h2>
-            <p>Project description</p>
-          </div>
-          <div className="bg-zinc-800 m-8 p-4 font-mont rounded col-start-2">
-            <h2 className="text-accent font-bold text-xl">Project #2</h2>
-            <p>Project description</p>
-          </div>
+    <section id="projectspage" className="lg:w-2/3 md:w-4/5 py-24 p-6 text-white mx-auto">
+      <h2 className="text-3xl text-center font-mont font-semibold text-accent">Projects</h2>
+      <p className="mt-4 lg:text-2xl lg:w-full md:max-w-2xl text-center mx-auto text-lg font-mont text-accent dark:text-white">
+        Create Project Component Next.
+      </p>
+      <div className='lg:grid lg:grid-cols-2 flex flex-col'>
+        <div className="bg-zinc-800 lg:m-8 lg:p-4 m-4 p-4 font-mont rounded">
+          <h2 className="text-accent font-bold text-xl">Project #1</h2>
+          <p>Project description</p>
         </div>
-      </section>
+        <div className="bg-zinc-800 m-8 p-4 font-mont rounded col-start-2">
+          <h2 className="text-accent font-bold text-xl">Project #2</h2>
+          <p>Project description</p>
+        </div>
+      </div>
+    </section>
+
     </>
   )
 }
